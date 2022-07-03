da719ec4286fb555717ad9bcf39384fdee126e9c
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-07-03 14:31:37 +0300

    Define op for planet details

--- app.py
+++ app.py
@@ -156,6 +156,16 @@ def retrieve_password(email: str):
         return jsonify(message="That email doesn't exist"), 401
 
 
+@app.route('/planet_details/<int:planet_id>', methods=["GET"])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet is not None:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
 # Database models
 class User(db.Model):
     # sqlalchemy table name
